991025ac029f24c1fdc9dc84465788c69046e2de
Mark Krensel <mark.krensel@gmail.com>
2022-10-24 15:30:04 +0200

    add api, add services

---
+++ pages/api/Days/index.js
@@ -0,0 +1,21 @@
+import dbConnect from "../../../lib/dbConnect";
+import Card from "../../../components/Card";
+import { getAllDays } from "../../../services/dayService";
+
+export default async function handler(request, response) {
+  if (request.method === "GET") {
+    const days = await getAllDays();
+    return response.status(200).json(days);
+  } else if (request.method === "POST") {
+    await dbConnect();
+
+    const postData = JSON.parse(request.body);
+    const newDay = await Day.create(postData);
+
+    return response
+      .status(201)
+      .json({ message: "Data saved", createdId: newDay.id });
+  }
+
+  return response.status(405).json({ message: "HTTP method is not allowed" });
+}
