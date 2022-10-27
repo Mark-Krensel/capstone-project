3e593d0e64a670cca15d0a0f02ed851d4d4bdb07
Mark Krensel <mark.krensel@gmail.com>
2022-10-27 23:44:44 +0200

    add stopwatch

---
+++ components/forms/Stopwatch.js
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+
+export default function Stopwatch() {
+  const [startTime, setStartTime] = useState();
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      const difference = now.getTime() - startTime;
+
+      const h = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      setHours(h);
+
+      const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      setMinutes(m);
+
+      const s = Math.floor((difference % (1000 * 60)) / 1000);
+      setSeconds(s);
+      localStorage.setItem("startTime", startTime);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [startTime]);
+  return (
+    <>
+      {startTime && (
+        <>
+          <div>hours{hours}</div>
+          <div>minutes{minutes}</div>
+          <div>seconds{seconds}</div>
+          <button
+            type="button"
+            aria-label="stop stopwatch"
+            onClick={() => {
+              setStartTime("");
+              localStorage.removeItem("startTime");
+            }}
+          >
+            Stop
+          </button>
+        </>
+      )}
+      {!startTime && (
+        <button
+          type="button"
+          aria-label="start stopwatch"
+          onClick={() => {
+            setStartTime(new Date().getTime());
+          }}
+        >
+          {" "}
+          Start
+        </button>
+      )}
+    </>
+  );
+}
