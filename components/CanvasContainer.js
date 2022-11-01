16c1c5e350bd71bf76114d98e99d7eb6a6fa5ec5
Mark Krensel <mark.krensel@gmail.com>
2022-11-01 22:13:19 +0100

    switch to react-chartjs-2 & made Linechart component

--- components/CanvasContainer.js
+++ components/CanvasContainer.js
@@ -2,12 +2,13 @@ import styled from "styled-components";
 
 const CanvasContainer = styled.div`
   max-height: 50vh;
+  width: 95vw;
   backdrop-filter: blur(10px);
-  width: 100%;
-
-  @media screen and (min-width: 800px) {
+  display: block;
+  margin: 3.5em auto 0 auto;
+  border-bottom: 1px solid var(--text-primary);
+  @media (min-width: 840px) {
     width: 800px;
-    margin: 0 80%;
   }
 `;
 
--- pages/weight.js
+++ pages/weight.js
@@ -2,9 +2,8 @@ import { CardContainer } from "../components/CardContainer";
 import Card from "../components/Card";
 import { getAllDays } from "../services/dayService";
 
-import React, { useEffect, useRef } from "react";
-import Chart from "chart.js/auto";
 import { CanvasContainer } from "../components/CanvasContainer";
+import LineChart from "../components/charts/LineChart";
 
 export async function getServerSideProps() {
   const days = await getAllDays();
@@ -14,75 +13,23 @@ export async function getServerSideProps() {
 }
 
 export default function WeightPage({ days }) {
-  const canvasEl = useRef(null);
-
   const filteredDays = days.filter((day) => Boolean(day.weight));
 
   const ascendingFilteredDays = Array.from(filteredDays).reverse();
 
-  console.table(filteredDays);
-  console.table(ascendingFilteredDays);
-
-  const colors = {
-    purple: {
-      default: "rgba(149, 76, 233, 1)",
-      half: "rgba(149, 76, 233, 0.5)",
-      quarter: "rgba(149, 76, 233, 0.25)",
-      zero: "rgba(149, 76, 233, 0)",
-    },
-    indigo: {
-      default: "rgba(80, 102, 120, 1)",
-      quarter: "rgba(80, 102, 120, 0.25)",
-    },
-  };
-
-  useEffect(() => {
-    const ctx = canvasEl.current.getContext("2d");
-
-    const gradient = ctx.createLinearGradient(0, 16, 0, 600);
-    gradient.addColorStop(0, colors.purple.half);
-    gradient.addColorStop(0.65, colors.purple.quarter);
-    gradient.addColorStop(1, colors.purple.zero);
-
-    const data = {
-      labels: ascendingFilteredDays.map(
-        (day) =>
-          `${day.date.toString().substr(8, 2)}.${day.date
-            .toString()
-            .substr(5, 2)}`
-      ),
-      datasets: [
-        {
-          backgroundColor: gradient,
-          label: "Weight",
-          data: ascendingFilteredDays.map((day) => day.weight),
-          fill: true,
-          borderWidth: 2,
-          borderColor: colors.purple.half,
-          lineTension: 0.3,
-          pointBackgroundColor: colors.purple.default,
-          pointRadius: 3,
-        },
-      ],
-    };
-    const config = {
-      type: "line",
-      data: data,
-    };
-    const myLineChart = new Chart(ctx, config);
-
-    return function cleanup() {
-      myLineChart.destroy();
-    };
-  });
+  const labels = ascendingFilteredDays.map(
+    (day) =>
+      `${day.date.toString().substr(8, 2)}.${day.date.toString().substr(5, 2)}`
+  );
+  const chartData = ascendingFilteredDays.map((day) => day.weight);
+  const title = "Weight";
 
   return (
     <>
+      <CanvasContainer>
+        <LineChart labels={labels} chartData={chartData} title={title} />
+      </CanvasContainer>
       <CardContainer>
-        <CanvasContainer>
-          <canvas id="myChart" ref={canvasEl} height="200" max-height="250" />
-        </CanvasContainer>
-
         {filteredDays.map((filteredDay) => (
           <Card
             key={filteredDay.id}
